c00cac38b67326a16e6d37b0ceeeebd513f7e25f
CesarOli <cesaraleoliveira@gmail.com>
2024-03-22 11:03:48 -0300

    Inicialização do projetoAlura, com desafio técnico proposto.

---
+++ projetoAlura/src/test/java/com/example/projetoAlura/ProjetoAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projetoAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
